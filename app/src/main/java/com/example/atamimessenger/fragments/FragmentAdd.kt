8ff209517e8164c6759a509b730141dca12ac108
LukaLominadze <l.lominadze2927@gmail.com>
2025-06-11 14:04:22 +0400

    adding fragments

---
+++ app/src/main/java/com/example/atamimessenger/fragments/FragmentAdd.kt
@@ -0,0 +1,27 @@
+package com.example.atamimessenger.fragments
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Button
+import android.widget.EditText
+import androidx.fragment.app.Fragment
+import com.example.atamimessenger.R
+
+class FragmentAdd : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        val view = inflater.inflate(R.layout.fragment_add, container, false)
+
+        return view
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+    }
+}
---
+++ app/src/main/java/com/example/atamimessenger/fragments/FragmentHome.kt
@@ -0,0 +1,25 @@
+package com.example.atamimessenger.fragments
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.example.atamimessenger.R
+
+class FragmentHome : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        val view = inflater.inflate(R.layout.fragment_home, container, false)
+
+        return view
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+    }
+}
